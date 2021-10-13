1969c36389cd282eb66c4aced23f1c1d616048db
DeadbraiN <deadbrainman@gmail.com>
2021-10-13 23:46:16 +0300

    removed extra struct

--- src/main.rs
+++ src/main.rs
@@ -10,35 +10,21 @@ use world::World;
 use vm::VM;
 use cfg::Config;
 
-pub struct App {
-    world: World,
-    vms: Vec<VM>
-}
-
 fn main() {
     //
     // We use %RUST_LOG% to set log level. Use SET RUST_LOG=<level> in
     // terminal to set log level. e.g. SET RUST_LOG=info
     //
     flexi_logger::Logger::try_with_env().unwrap().start().unwrap();
+    info!("Welcome to irma4 - Atomic Artificial Life Simulator in Rust");
     //
-    // Global configuration. Must be singleton
+    // Global configuration. Must be a singleton
     //
     let mut cfg = Config::new();
-    let mut app: App = App {
-        world: World::new(cfg.WIDTH() * cfg.HEIGHT()),
-        vms: VM::create_vms(cfg.VM_AMOUNT(), cfg.MOV_BUF_SIZE())
-    };
+    let mut world = World::new(cfg.WIDTH() * cfg.HEIGHT());
+    let mut vms = VM::create_vms(cfg.VM_AMOUNT(), cfg.MOV_BUF_SIZE());
     //
     // TODO: 
     //
-    let index = cfg.WIDTH() * cfg.HEIGHT() - 1;
-    info!("Welcome to irma4 - Artificial life simulator on Rust");
-    info!("Allocated memory: {}. Value[{}]: {}", app.world.len(), index, app.world.get_dot(index));
-    cfg.frame_delay = 123;
-    println!("{}", cfg.frame_delay);
-    //
-    // TODO:
-    //
-    app.vms[0].step(&app.world);
+    vms[0].step(&world);
 }
